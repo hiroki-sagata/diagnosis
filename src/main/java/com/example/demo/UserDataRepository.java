b5bba9d0df2204b683b60e81468e405a4eb00e30
hiroki-sagata <seve.seas8881@icloud.com>
2021-02-02 14:09:31 +0900

    mysql

--- src/main/java/com/example/demo/UserDataRepository.java
+++ src/main/java/com/example/demo/UserDataRepository.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface UserDataRepository extends
 JpaRepository<UserData, Long>{
+	
+	//↓↓↓--------------mysql接続用に作成----------------↓↓↓
 
+//public interface UserRepository extends CrudRepository<UserData,Long>{}
+	
+	
+	
 	
 	//↓↓↓--------------spring_security用に作成----------------↓↓↓
 
